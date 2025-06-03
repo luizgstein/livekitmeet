7f8a0152ac7c9b1761cdd7bb6ae2354b4beeff4f
luizgstein <luizgstein@gmail.com>
2025-06-02 21:26:08 -0300

    Update CameraSettings.tsx

--- lib/CameraSettings.tsx
+++ lib/CameraSettings.tsx
@@ -8,14 +8,9 @@ import {
 } from '@livekit/components-react';
 import { BackgroundBlur, VirtualBackground } from '@livekit/track-processors';
 import { isLocalTrack, LocalTrackPublication, Track } from 'livekit-client';
-import Desk from '../public/background-images/samantha-gades-BlIhVfXbi9s-unsplash.jpg';
-import Nature from '../public/background-images/ali-kazal-tbw_KQE3Cbg-unsplash.jpg';
 
 // Background image paths
-const BACKGROUND_IMAGES = [
-  { name: 'Desk', path: Desk },
-  { name: 'Nature', path: Nature },
-];
+const BACKGROUND_IMAGES: any[] = [];
 
 // Background options
 type BackgroundType = 'none' | 'blur' | 'image';
